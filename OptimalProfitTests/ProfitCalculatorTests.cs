371ef2f51b8745ac4d3fbf3fec62331401bde6ab
Benjamin Tse <benjamin.l.tse@gmail.com>
2019-05-09 10:20:19 +1000

    Add extra test cases

--- OptimalProfitTests/ProfitCalculatorTests.cs
+++ OptimalProfitTests/ProfitCalculatorTests.cs
@@ -9,6 +9,13 @@ namespace OptimalProfitTests
         [Theory]
         [InlineData(new[] {5, 10}, 5)]
         [InlineData(new[] {10, 7, 5, 8, 11, 9}, 6)]
+        [InlineData(new int[] {}, 0)]
+        [InlineData(new[] {1}, 0)]
+        [InlineData(new[] {10, 10, 10}, 0)]
+        [InlineData(new[] {10, 9, 8 }, 0)]
+        [InlineData(new[] {10, 9, 0 }, 0)]
+        [InlineData(new[] {10, 9, 5, 2, 8, 9, 14, 19, 0 }, 17)]
+        [InlineData(new[] {10, 9, 2, 20, 28, 3, 26 }, 26)]
         public void CalculateProfits(int[] stockPrices, int optimalProfit)
         {
             var profitCalculator = new DummyProfitCalculator();
